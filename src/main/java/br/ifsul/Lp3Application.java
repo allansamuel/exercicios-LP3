24f9a5b070dec966dd4cfdda8557239824e8b4f6
alunoinfo <alunoinfo@LAB08M21.ifsul-sapucaia.local>
2020-03-09 11:54:17 -0300

    Exercicio 3

--- src/main/java/br/ifsul/Lp3Application.java
+++ src/main/java/br/ifsul/Lp3Application.java
@@ -2,53 +2,44 @@ package br.ifsul;
 
 import java.sql.Date;
 
-import javax.swing.JOptionPane;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Lp3Application {
+public class Lp3Application extends UIDespesa {
 
 	@Autowired
 	private DespesaRepository dr;
-	
-	private static void criaDespesa(Lp3Application app) {
-		String descricao = JOptionPane.showInputDialog(null, "Insira uma descrição");
-		String valor = JOptionPane.showInputDialog(null, "Insira um valor");
-		String data = JOptionPane.showInputDialog(null, "Insira uma data");
-		
-		Date d = Date.valueOf(data);
-		Float f = Float.valueOf(valor);
-		
-		Despesa despesa = new Despesa(descricao, f, d);
-		app.dr.save(despesa);
-	}
-	
+
 	public static void main(String[] args) throws Throwable {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(Lp3Application.class);
-		builder.headless(false);
-		ApplicationContext c = builder.run(args);
-		Lp3Application app = c.getBean(Lp3Application.class);
-		
-		String opcao = JOptionPane.showInputDialog(null, "Informe um valor para a próxima operação:\n1 - Nova despesa\n2 - Listar despesas\n3 - Sair da aplicação");
-		
-		while(!(opcao.equals("1") && opcao.equals("2"))) {
-			opcao = JOptionPane.showInputDialog(null, "Informe um valor para a próxima operação:\n1 - Nova despesa\n2 - Listar despesas\n3 - Sair da aplicação");
-			if(opcao.equals("1")) {
-				criaDespesa(app);
-			}else if(opcao.equals("2")) {
-				for(Despesa d : app.dr.findAll()) {
-					System.out.println("------DESPESA #" + d.getId() + "--------\n" + d.getDescricao() + "\n" + d.getValor() + "\n" + d.getData() + "\n");
-				}
-			}else {
-				return;
-			}
-		}
-		
+		SpringApplication.run(Lp3Application.class, args);
+
 	}
+
+	@Override
+	protected void onNovaDespesa() {
+		String des = txtDescricaoDespesa.getText();
+		String val = txtValorDespesa.getText();
+		String dat = txtDataDespesa.getText();
+
+		Despesa d = new Despesa();
+		d.setDescricao(des);
+		d.setData(Date.valueOf(dat));
+		d.setValor(Float.parseFloat(val));
+
+		d = dr.save(d);
+
+		modelDespesas.addElement(d);
+	}
+
+	@Override
+	protected void onRemoverDespesa(Despesa selecionado) {
+		dr.delete(selecionado);
+		modelDespesas.removeElement(selecionado);
+
+	}
+	
 	
 
 }
